7496271c519258d3a9d50c6244794c7af3cd8d6b
Paweł Grzesiewicz <p.grzesiewicz@gmail.com>
2024-01-24 13:55:12 +0100

    added forgotten alt for img

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     <div className={`gpt3__navbar ${isMoved ? 'is-moved' : ''}`}>
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt='logo'/>
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
